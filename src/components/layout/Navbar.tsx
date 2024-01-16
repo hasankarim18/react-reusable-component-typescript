79b6fb5b19385250b95b2b12059a7976bc0aeb59
hasankarim18 <hallysust@gmail.com>
2024-01-16 18:08:48 +0600

    typescript react vite+tailwind+react-router-dom

---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <div>
+      <h1 className="text-3xl w-full py-2 bg-gray-400 text-white text-center">
+        Navbar
+      </h1>
+    </div>
+  );
+};
+
+export default Navbar;
